c2e300b852710cfd5d90dbc44f111682f015a84b
mstruve <mollylbs@gmail.com>
2020-09-28 11:41:48 -0500

    rm more webdriver lines

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -1,8 +1,7 @@
 require "capybara/rails"
 require "capybara/rspec"
-require "webdrivers/chromedriver"
 
-Webdrivers.cache_time = 86_400
+# Webdrivers.cache_time = 86_400
 
 Capybara.default_max_wait_time = 10
 
